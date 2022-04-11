87b0acc1b0cdf2b7e887e1da051450343b669527
Li Xu <luckylixu@gmail.com>
2022-04-11 16:01:03 +0000

    first commit

---
+++ load_gefs_benchmark.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  1 00:57:42 2022
+@author: li.xu
+
+contact:
+    Li Xu
+    CPC/NCEP/NOAA
+    (301)683-1548
+    li.xu@noaa.gov
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+
+import xu
+from xu import ndates,Ds,mmean,rmse,bias,cf,atxt
+
+
+#EFSv12 reforecast:
+
+f1='/cpc/gth/GTH_DATABASE/WEEK2_HEAT/GEFS_SEHOS/GEFS_v12_reforecast_HeatIndex.nc'
+f2='/cpc/gth/GTH_DATABASE/WEEK2_HEAT/GEFS_SEHOS/GEFS_v12_reforecast_airT.nc'
+
+
+
+#CDAS reanalysis:
+
+o2='/cpc/home/evan.oswald/R1_SEHOS/CDAS_1x1_reanalysis_airT.nc'
+o1='/cpc/home/evan.oswald/R1_SEHOS/CDAS_1x1_reanalysis_HeatIndex.nc'
+
+
+
+dso1=Ds(o1)
+dsf1=Ds(f1)
+
+#dsf1=dsf1.set_coords('issue_date')
+#dso1=dso1.set_coords('issue_date')
+
+lat=dsf1.latitude.values
+lon=dsf1.longitude.values
+
+to=dso1.issue_date.values.astype('i4')
+tf=dsf1.issue_date.values.astype('i4')
+
+
+f=dsf1.max_heat_index_2m.values
+f=mmean(f,2)  #[71,31,7,5280]
+
+o=dso1.max_heat_index_2m.values
+o=np.flip(o,axis=1) # to make latitude from low to high, the same as fcst
+
+
+
+tfn=tf+7 #day8
+
+select=np.in1d(to,tfn) # to get same issue_date at observation with forecast
+o8=o[:,:,select]
+
+bias8=bias(f[:,:,0,:],o8,axis=2)
+rmse8=rmse(f[:,:,0,:],o8,axis=2)
+
+fc=f[:,:,0,:]-bias8[:,:,np.newaxis]
+
+rmse8_fc=rmse(fc,o8,axis=2)
+
+
+lev=[-5,-4,-3,-2,-1,1,2,3,4,5]
+
+cf(lon,lat,bias8.T,lev,extend='both',title='bias for day8 \n',l='heat index',r='unit:F')
+m=mmean(bias8,(0,1))
+atxt(-75,30,f'mean={m:.2f}')
+
+lev=[2,4,6,8,10,12,14,16,18]
+lev=np.arange(15)
+cf(lon,lat,rmse8.T,lev,cm='Accent',extend='both',title='RMSE for day8 \n',l='heat index',r='unit:F')
+cf(lon,lat,rmse8_fc.T,lev,cm='Accent',extend='both',title='RMSE for day8 \n',l='heat index',r='unit:F')
+
+
+
+def p_rmse(M,title):
+    lev=np.arange(1,15)
+    cf(lon,lat,M.T,lev,cm='YlGnBu',extend='both',title=title,l='heat index',r='unit:F')
+    m=mmean(M,(0,1))
+    atxt(-75,30,f'mean={m:.2f}')
+
+p_rmse(rmse8,'RMSE for day8 \n')
+p_rmse(rmse8_fc,'RMSE after BC for day8 \n')
+
+
+for i in range(7): #day8-14  start from 0
+
+
+    tfn=tf+7+i #day
+
+    select=np.in1d(to,tfn) # to get same issue_date at observation with forecast
+    oday=o[:,:,select]
+
+    bias_day=bias(f[:,:,i,:],oday,axis=2)
+    rmse_day=rmse(f[:,:,i,:],oday,axis=2)
+
+    fc=f[:,:,i,:]-bias_day[:,:,np.newaxis]
+
+    rmse_fc=rmse(fc,oday,axis=2)
+
+
+    lev=[-5,-4,-3,-2,-1,1,2,3,4,5]
+
+    cf(lon,lat,bias_day.T,lev,extend='both',title=f'bias for day{i+7+1} \n',l='heat index',r='unit:F')
+    m=mmean(bias_day,(0,1))
+    atxt(-75,30,f'mean={m:.2f}')
+
+    def p_rmse(M,title):
+        lev=np.arange(1,15)
+        cf(lon,lat,M.T,lev,cm='YlGnBu',extend='both',title=title,l='heat index',r='unit:F')
+        m=mmean(M,(0,1))
+        atxt(-75,30,f'mean={m:.2f}')
+
+    p_rmse(rmse_day,f'RMSE for day{i+7+1} \n')
+    p_rmse(rmse_fc,f'RMSE after BC for day{i+7+1} \n')
+
+
+
+
+
+def M2V(D):
+    '''convert from 2D Matrix to 1D vector, eliminate all undefined ocean
+    [...,31,71] to [...,1368]'''
+    mask=np.load('mask.npy')
+    return D[...,mask]
+
+
+
+def V2M(V):
+    '''convert from 1D vector to 2D matrix for map, fill ocean with undefined'''
+
+    def npa(shape=(60, 150), default=np.nan):
+        """create a float32 ndarray with nan inital"""
+        # return np.zeros(shape, dtype='float32') * np.nan
+        return np.full(shape, default, dtype="float32")
+
+    mask=np.load('mask.npy')
+    if V.shape[-1] == 1368:
+        M = npa((*V.shape[:-1], 31, 71))
+        M[..., mask] = V
+        return M
+    else:
+        print('wrong shape (need to be 1368 at last dim)')
+        return None
+
+
+def test():
+    x=M2V(D.T)
+    N=V2M(x)
+
+
+
+
+def get_mask():
+    D=dsf1['max_heat_index_2m'].values
+    M=D[:,:,0,0,0].T
+    mask=~np.isnan(M)  #total 1368 land points
+    np.save('mask.npy',mask)
+
+
+def num2date(n):
+    from datetime import datetime
+    from datetime import timedelta
+    start=datetime.strptime('00010101','%Y%m%d')
+    date=start+timedelta(days=n)
+    return date.strftime('%d%h%Y')
+
+
+
+
+def bias():
+
+
+
+def main(yyyymmdd):
+    print(yyyymmdd)
+
+
+if __name__ == '__main__':
+    import sys
+    import fire
+    if len(sys.argv) <= 1:
+        print('input: YYYYMMDD or -h')
+    else:
+        if len(sys.argv[1]) == 8:
+            main(sys.argv[1])
+        else:
+            fire.Fire()
---
+++ xu.py
@@ -0,0 +1,128 @@
+
+# checked file: load_gefs_benchmark.py
+# from xu import ndates,Ds,mmean,rmse,bias,cf,atxt
+
+# xu function set: {'atxt', 'rmse', 'cf', 'ndates', 'Ds', 'mmean', 'bias'}
+
+import numpy as np
+import matplotlib.pyplot as plt
+def atxt(x=0, y=-.15, txt='xxx', **kwargs):
+    '''plot txt at axes (x,y) axes coodinate'''
+    plt.gca().text(x, y, txt, **kwargs)
+
+def rmse(D, Dtrue, axis=0):
+    '''return RMSE along axis(default 0, time, could be tuple)'''
+    A = D - Dtrue
+    return np.sqrt(mmean(A * A, meanaxis=axis))
+
+def cf(
+        lon,
+        lat,
+        M,
+        lev=25,
+        cm='bwr',
+        title='',
+        l='',
+        r='',
+        cbar=True,
+        cax=[0, -0.05, 1, 0.03],  #[left, bottom, width, height]
+        bottom=0,
+        clabel='',
+        cticks=False,
+        figsize=(6, 4.5),
+        extent=None,
+        aspect=1.3,
+        cyclic=False,
+        omask=False,
+        lmask=False,
+        **kwargs):
+    '''kwargs:
+    norm=n,
+    alpha=0.8,
+    extend='both','max','min' 'neither'
+    extent=[-125, -65, 25, 45.7]
+    cax=[1.02, 0.0, 0.03, 1] for horizontal cbar
+             '''
+    import cartopy.crs as ccrs
+    from xu import lat_lon_ticklabels
+    #plt.switch_backend('agg')
+
+    if cyclic:
+        from cartopy.util import add_cyclic_point
+        M, lon = add_cyclic_point(M, coord=lon)
+
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree(), aspect=aspect)
+    CS = ax.contourf(lon, lat, M, levels=lev, cmap=cm, **kwargs)
+
+    import cartopy.feature as cfeature
+    if omask:
+        ax.add_feature(cfeature.OCEAN, facecolor='white', zorder=1)
+    if lmask:
+        ax.add_feature(cfeature.LAND, facecolor='lightgray', zorder=1)
+
+    # ax.coastlines(resolution='50m')
+    ax.coastlines()
+    ax.add_feature(cfeature.BORDERS, linewidth=1, alpha=0.8)
+    ax.add_feature(cfeature.STATES, linestyle=':', lw=0.5)
+
+    if title: ax.set_title(title)
+    if l: ax.set_title(l, loc='left')
+    if r: ax.set_title(r, loc='right')
+    if bottom: fig.subplots_adjust(bottom=bottom)
+
+    if cbar:
+        #cb_ax = fig.add_axes([0.15, 0.16, 0.73,0.03])  #[left, bottom, width, height]
+        cb_ax = ax.inset_axes(cax, transform=ax.transAxes)
+        cbar = plt.colorbar(CS,
+                            cax=cb_ax,
+                            orientation='horizontal',
+                            extendrect=False,
+                            extendfrac='auto',
+                            label=clabel)
+
+    if cticks:
+        cbar.set_ticks(lev)
+        cbar.set_ticklabels(lev)
+
+    if extent:
+        ax.set_extent(extent, crs=ccrs.PlateCarree())
+
+    return ax
+
+def ndates(nhours=0, newdate=None, fmt='%Y%m%d') -> str:
+    ''' improved ndate()  fmt='%Y%m%d%H', defulat 00z
+    return str,  accord to fmt='%Y %m %d %H %h' '''
+    from datetime import datetime
+    from datetime import timedelta
+    if newdate is None:
+        today = datetime.now()
+    else:
+        try:
+            today = datetime.strptime(newdate, '%Y%m%d')
+        except:
+            print('error date:', newdate)
+            today = datetime.strptime(newdate[:6] + '28', '%Y%m%d')
+
+    delta = today + timedelta(hours=nhours)
+    return delta.strftime(fmt)
+
+def Ds(ds_file="data/nldas_smp.nc"):
+    """return dataset from nc_file, by xarray
+    need to be "good" nc"""
+    import xarray as xr
+
+    if ds_file.endswith(("grb", "grb2")):
+        Ds = xr.open_dataset(ds_file, engine="cfgrib")
+    else:
+        Ds = xr.open_dataset(ds_file)
+    Ds  # display information
+    return Ds
+
+def mmean(D, meanaxis=0, keepdims=False):
+    """meanaxis   {int, tuple of int, None}, axis=-1 the last dim """
+    return np.nanmean(D, axis=meanaxis, keepdims=keepdims)
+
+def bias(Df,Do,axis=0):
+    return mmean(Df,axis)-mmean(Do,axis)
+
